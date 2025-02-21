2d36179e1e08285a8fdae990d577bf978495258d
Vincent <vincent.brunelle.gm@gmail.com>
2025-02-21 13:56:34 -0500

    ADD : Fetching every evolutions of a given pokemon

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
+const POKEMON_TEST = "bulbasaur";
+
 window.onload = async () => {
   const list = await getPokemonListNames(151);
-  console.log(list);
-  const pokemonData = await getSpecificPokemon("ivysaur");
-  console.log(pokemonData);
-  const pokemonType = getPokemonTypes(pokemonData);
-  console.log(pokemonType);
+  const pokemonData = await getSpecificPokemon(POKEMON_TEST);
+  const pokemonSpecies = await getSpecie(pokemonData);
+  const pokemonEvolutionChain = await getEvolutionChain(pokemonData);
+  const evolutionChain = await getNextEvolutions(pokemonData);
+  console.log(evolutionChain);
 };
 
-async function fetchPokemons(numberOfPokemons = 151) {
+async function fetchData(url) {
   try {
-    const response = await fetch(
-      "https://pokeapi.co/api/v2/pokemon?limit=" + numberOfPokemons
-    );
+    const response = await fetch(url);
     if (!response.ok) {
       handleFetchResponseError(response);
     }
     const data = await response.json();
-    console.log(data);
-    const pokemons = data.results;
-    return pokemons;
+    return data;
   } catch (error) {
-    //Handle every error from the data fetching
     handleError(error);
   }
+}
+
+async function fetchPokemons(numberOfPokemons = 151) {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${numberOfPokemons}`;
+  const data = await fetchData(url);
+  return data.results;
 }
 
 async function getPokemonListNames(numberOfPokemons) {
@@ -36,29 +39,58 @@ async function getPokemonListNames(numberOfPokemons) {
 }
 
 function handleError(error) {
-  console.log(error);
+  //console.log(error);
   //TODO : return error page
 }
 
 function handleFetchResponseError(response) {
-  console.log("Error fetching data" + response.status);
+  //console.log("Error fetching data" + response.status);
 }
 
 async function getSpecificPokemon(pokemonName) {
-  try {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
-    if (!response.ok) {
-      handleFetchResponseError(response);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    handleError(error);
-  }
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  const data = await fetchData(url);
+  return data;
 }
 
 function getPokemonTypes(pokemonData) {
   return pokemonData.types;
+}
+
+async function getSpecie(pokemonData) {
+  const speciesUrl = pokemonData.species.url;
+  console.log(speciesUrl);
+  const speciesData = await fetchData(speciesUrl);
+  return speciesData;
+}
+
+async function getEvolutionChain(pokemonData) {
+  const speciesData = await getSpecie(pokemonData);
+  const ecolutionChainURL = speciesData.evolution_chain.url;
+  const evolutionChainData = await fetchData(ecolutionChainURL);
+  return evolutionChainData.chain;
+}
+
+async function getNextEvolutions(pokemonData, evolutionsFound = []) {
+  const pokemonEvolutionChain = await getEvolutionChain(pokemonData);
+
+  //Recursive function to visit every evolutions
+  async function visitChain(chain) {
+    if (chain.evolves_to.length === 0) {
+      return;
+    }
+
+    for (const evolution of chain.evolves_to) {
+      const evolutionName = evolution.species.name;
+
+      if (!evolutionsFound.includes(evolutionName)) {
+        evolutionsFound.push(evolutionName);
+        await visitChain(evolution);
+      }
+    }
+  }
+
+  await visitChain(pokemonEvolutionChain);
+
+  return evolutionsFound;
 }
